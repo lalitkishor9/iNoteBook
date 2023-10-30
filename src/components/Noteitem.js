67bb17290e879361810dc5a803818a1a94ac9192
Lalit Kishor <lalitkishor202125@gmail.com>
2023-10-30 11:21:14 +0530

    final changes

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -11,7 +11,7 @@ function Noteitem(props) {
       <div class="card-body">
         <h4 class="card-title">{notes.title}</h4>
         <h6 class="card-subtitle mb-2">{notes.tag}</h6>
-        <p class="card-text">Apart from being regarded as the City of Music because of its musical legacy, it is also said to be "The City of Dreams".</p>
+        <p class="card-text">{notes.description}</p>
           <div className="d-flex flex-row-reverse">
             <i className="fa-regular fa-trash-can fa-xl mx-4 " style={{color:"red"}} onClick={()=>{deleteNote(notes._id); props.showAlert("Deleted Successfully", "success");}} ></i>
             <i className="fa-regular fa-pen-to-square fa-xl " onClick={()=>{updateNote(notes); }} style={{color:"blue"}} ></i>
